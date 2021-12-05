6092c71ad261c30e31583d6d3c334ed944f21a18
채윤병 <ybchae0801@naver.com>
2021-12-05 18:51:20 +0900

    section_3: Implement xml type metadata reading
    - JAXB를 사용해 XML 메타데이터 다루기
    - JUnit을 사용해 테스트 코드 개선하기

---
+++ src/test/java/moviebuddy/domain/JaxbMovieReaderTest.java
@@ -0,0 +1,17 @@
+package moviebuddy.domain;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+class JaxbMovieReaderTest {
+
+    @Test
+    void NotEmpty_LoadedMovies() {
+        JaxbMovieReader movieReader = new JaxbMovieReader();
+        List<Movie> movies = movieReader.loadMovies();
+        Assertions.assertEquals(1375, movies.size());
+    }
+
+}
