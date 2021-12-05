3819ec2857059aa2adc785036c332bec4637e8ac
채윤병 <ybchae0801@naver.com>
2021-12-05 20:24:49 +0900

    section_8: Refactor testing with test-context framework
    - 스프링 통합 테스트 지원
    - 영화 검색 컴포넌트 테스트를 스프링 통합 테스트로 변경하기

--- src/test/java/moviebuddy/domain/JaxbMovieReaderTest.java
+++ src/test/java/moviebuddy/domain/JaxbMovieReaderTest.java
@@ -1,11 +1,21 @@
 package moviebuddy.domain;
 
+import moviebuddy.MovieBuddyFactory;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
 import java.util.List;
 
+@SpringJUnitConfig(MovieBuddyFactory.class)
 class JaxbMovieReaderTest {
+
+    final MovieReader movieReader;
+
+    public JaxbMovieReaderTest(@Qualifier("jaxbMovieReader") MovieReader movieReader) {
+        this.movieReader = movieReader;
+    }
 
     @Test
     void NotEmpty_LoadedMovies() {
